a66b73cb767747d0586bcee7a46eaaf2b95a28a0
Mia Tran <maiptran157@gmail.com>
2018-09-19 17:29:20 -0700

    Complete delete item functionality

---
+++ src/components/item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default (props) => {
+    return <li className="collection-item">
+        <div className="col s8"> {props.item.title}</div>
+        <div className="col s4 right-align">
+            <button className="btn red darken-2" onClick={props.delete}>Delete</button></div>
+    </li>
+};
